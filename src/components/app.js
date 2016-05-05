9ad928d337c5343ef62230458ccb99f1bc9d9080
tigerbombz <madd1778@yahoo.com>
2016-05-05 12:17:55 -0700

    added routes, nav, footer, and contentIndex page

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
+import Nav from './layout/nav';
+import Footer from './layout/footer';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Nav />
+        {this.props.children}
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/components/content_index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAppts } from '../actions/index'
+import { Link } from 'react-router';
+
+
+class ContentIndex extends Component {
+
+  render() {
+    return (
+      <div>
+        <div className='main_div'>
+          <img src={'https://www.salonpricelady.com/wp-content/uploads/2016/02/hair-salon-inside.jpg'} alt='main'className='main' />
+        </div>
+        <div className='quote'>
+          <span className='words'>
+            THE ULTIMATE EXPERIENCE IN LUXURY HAIR CARE
+          </span>
+        </div>
+        <div className='image_container'>
+          <div className='box col-md-3 col-sm-3'>
+            <img src={'http://absolutbusinessbrokers.com.au/wp-content/uploads/2015/03/hair-salon-business-fro-sale-B11983.jpg'} style={{width: 330, height: 250}} />
+            <label> Hair Color </label>
+          </div>
+          <div className='box col-md-3 col-sm-3'>
+            <img src={'http://arsengurgov.com/wp-content/uploads/2016/02/dry-hair-cut-Arsen-Gurgov-NYC-Hairstylist-.jpg'} style={{width: 330, height: 250}} />
+            <label> Haircut & Style </label>
+          </div>
+          <div className='box col-md-3 col-sm-3'>
+            <img src={'http://im.hunt.in/cg/panipat/City-Guide/beauty-parlour-1.jpg'} style={{width: 330, height: 250}} />
+            <label> Beauty Service </label>
+          </div>
+          <div className='box col-md-3 col-sm-3'>
+            <img src={'http://providenceplacebridal.com/wp-content/uploads/2015/04/wedding-hair-2.jpg'} style={{width: 330, height: 250}} />
+            <label> Bridal</label>
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchAppts })(ContentIndex);
