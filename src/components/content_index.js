013fbb85f423c5668c4864535753a52630081746
tigerbombz <madd1778@yahoo.com>
2016-08-17 11:13:21 -0700

    added hyperlinks to services for clickable actions on mobile

--- src/components/content_index.js
+++ src/components/content_index.js
@@ -23,19 +23,20 @@ class ContentIndex extends Component {
         <div className='image_container'>
           <div className='box col-md-3 col-sm-3'>
             <Link to='services'><img src={'http://absolutbusinessbrokers.com.au/wp-content/uploads/2015/03/hair-salon-business-fro-sale-B11983.jpg'} style={{width: 340, height: 250}} /></Link>
-            <label> Hair Color </label>
+            <Link to='services'><label> Hair Color </label></Link>
+
           </div>
           <div className='box col-md-3 col-sm-3'>
             <Link to='services'><img src={'http://arsengurgov.com/wp-content/uploads/2016/02/dry-hair-cut-Arsen-Gurgov-NYC-Hairstylist-.jpg'} style={{width: 340, height: 250}} /></Link>
-            <label> Haircut & Style </label>
+            <Link to='services'><label> Haircut & Style </label></Link>
           </div>
           <div className='box col-md-3 col-sm-3'>
             <Link to='services'><img src={'http://im.hunt.in/cg/panipat/City-Guide/beauty-parlour-1.jpg'} style={{width: 340, height: 250}} /></Link>
-            <label> Beauty Service </label>
+            <Link to='services'><label> Beauty Service </label></Link>
           </div>
           <div className='box col-md-3 col-sm-3'>
             <Link to='services'><img src={'http://providenceplacebridal.com/wp-content/uploads/2015/04/wedding-hair-2.jpg'} style={{width: 340, height: 250}} /></Link>
-            <label> Bridal</label>
+            <Link to='services'><label> Bridal</label></Link>
           </div>
 
         </div>
